2fdcfdc7e513904b3d22d6153da749420aa2e3f8
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-10-31 14:47:36 +0100

    Added mock namespace method and changed psm lookup to yielding all from multiple fractions

--- readers.py
+++ readers.py
@@ -9,6 +9,19 @@ def readPercout(fname):
         sys.stderr.write('Could not parse XML provided in %s or error reading file. \n' % (fname))
     return doc
 
+def get_namespace(fn):
+    ns = {'xmlns':'http://per-colator.com/percolator_out/14',
+    'xmlns:p' : 'http://per-colator.com/percolator_out/14',
+    'xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance',
+    'xsi:schemaLocation':'http://per-colator.com/percolator_out/14,https://github.com/percolator/percolator/raw/pout-1-4/src/xml/percolator_out.xsd',
+    'p:majorVersion':'0',
+    'p:minorVersion':'00',
+    'p:percolator_version':'Percolator version 0.00'
+    }
+    return ns
+    # FIXME lookup from file
+
+
 def get_percolator_static_xml(fn, ns):
     rootgen = etree.iterparse(fn, tag='{%s}percolator_output' % ns['xmlns'], events=('start',))
     root = rootgen.next()[1]
@@ -19,8 +32,10 @@ def get_percolator_static_xml(fn, ns):
     return root
 
 
-def get_psms(fn, ns):
-    return etree.iterparse(fn, tag='{%s}psm' % ns['xmlns'])
+def generate_psms_multiple_fractions(fns, ns):
+    for fn in fns:
+        for ac,el in etree.iterparse(fn, tag='{%s}psm' % ns['xmlns']):
+            yield el
     
 def get_peptides_multiple_fractions(input_files, ns):
     peptide_generators_fractions = []
