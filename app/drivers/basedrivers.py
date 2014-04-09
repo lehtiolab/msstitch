ab273eb5dd879ad071ca67efb4ed90e88b773089
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-04-09 11:08:00 +0200

    Created a basedrivers module. Input files will be single files from here

---
+++ app/drivers/basedrivers.py
@@ -0,0 +1,45 @@
+import os
+
+from app.readers import pycolator as readers
+from app.writers import pycolator as writers
+
+
+class BaseDriver(object):
+    def __init__(self, **kwargs):
+        self.fn = kwargs['infile']
+        self.outdir = kwargs['outdir']
+        self.outsuffix = kwargs.get('outsuffix', '.xml')
+
+    def create_outfilepath(self, fn, suffix=None):
+        basefn = os.path.basename(fn)
+        outfn = basefn + suffix
+        return os.path.join(self.outdir, outfn)
+
+
+class PycolatorDriver(object):
+    """Driver for pycolator functions"""
+    def prepare_percolator_output(self, fn):
+        """Returns namespace and static xml from percolator output file"""
+        ns = readers.get_namespace(fn)
+        static = readers.get_percolator_static_xml(fn, ns)
+        return ns, static
+
+    def get_all_peptides(self):
+        return readers.generate_peptides_multiple_fractions(self.fn, self.ns)
+
+    def get_all_psms(self):
+        return readers.generate_psms_multiple_fractions(self.fn, self.ns)
+
+    def prepare(self, fn):
+        self.ns, self.static_xml = self.prepare_percolator_output(fn)
+        self.allpeps = self.get_all_peptides()
+        self.allpsms = self.get_all_psms()
+
+    def run(self):
+        self.prepare(self.fn)
+        self.set_features()
+        self.write(self.fn)
+
+    def write(self):
+        outfn = self.create_outfilepath(self.fn, self.outsuffix)
+        writers.write_percolator_xml(self.static_xml, self.features, outfn)
