da4f40978b66bf8805a07ecb1e06d705c2bde99b
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-05-14 14:40:47 +0200

    Wrote a tsv writer for percolator

--- app/writers/pycolator.py
+++ app/writers/pycolator.py
@@ -8,7 +8,8 @@ def write_percolator_xml(staticxml, feats, fn):
 
     # First get xml until psms opening element is found.
     etree.SubElement(staticxml, 'psms').text = '***psms***'
-    root = etree.tostring(staticxml, pretty_print=True, xml_declaration=True, encoding='UTF-8')
+    root = etree.tostring(staticxml, pretty_print=True,
+                          xml_declaration=True, encoding='UTF-8')
     root = root.decode('utf-8')
     root = root[:root.find('***psms***')]
 
@@ -32,8 +33,8 @@ def write_percolator_xml(staticxml, feats, fn):
             fp.write(pep)
             fp.write('\n')
         fp.write('</peptides></percolator_output>')
-    print(('Wrote {0} psms, {1} peptides to file {2}'.format(psmcount,
-                                                            peptidecount, fn)))
+    print('Wrote {0} psms, {1} peptides to file {2}'.format(psmcount,
+                                                            peptidecount, fn))
 
 
 def write_qvality_input(scores, fn):
@@ -42,30 +43,18 @@ def write_qvality_input(scores, fn):
             fp.write('{0}\n'.format(score))
 
 
-def outputTabSep(fn, to_process, outputfn, ns):
-    features = etree.iterparse(fn, tag='{%s}%s' % (ns['xmlns'], to_process) )
-    header = {'psm':['ID', 'score', 'q-value', 'PEP', 'peptide', 'protein IDs\n'],
-          'peptide':['ID', 'score', 'q-value', 'PEP', 'psm_ids', 'protein IDs\n']
-          }
-    with open(outputfn, 'w') as fp:
-        fp.write('\t'.join(header[to_process]))
-        for ac,el in features:
-            p_id = el.attrib['{%s}%s_id' % (ns['xmlns'], to_process)]
-            score = el.xpath('xmlns:svm_score', namespaces=ns)[0].text
-            q = el.xpath('xmlns:q_value', namespaces=ns)[0].text
-            PEP = el.xpath('xmlns:pep', namespaces=ns)[0].text
-            proteins = '; '.join( [x.text for x in el.xpath('xmlns:protein_id',
-                                    namespaces=ns) ] )
+def write_percolator_tsv(lines, feattype, outfn):
+    linekeys = {'psm': ['p_id', 'score', 'q', 'PEP', 'pepseq', 'proteins'],
+                'peptide': ['p_id', 'score', 'q', 'PEP', 'psm_ids', 'proteins']
+                }
+    header = {'psm': ['ID', 'svm-score', 'q-value', 'PEP',
+                      'peptide', 'protein IDs'],
+              'peptide': ['ID', 'svm-score', 'q-value', 'PEP',
+                          'psm_ids', 'protein IDs']
+              }
 
-            if to_process == 'peptide':
-                psm_ids = '; '.join([x.text for x in el.xpath('xmlns:psm_id',
-                                    namespaces=ns) ])
-                fp.write('\t'.join([p_id, score, q, PEP, psm_ids, proteins]))
-
-            elif to_process == 'psm':
-                pepseq = el.xpath('xmlns:peptide_seq',
-                        namespaces=ns)[0].attrib['seq']
-                fp.write('\t'.join([p_id, score, q, PEP, pepseq, proteins]))
+    with open(outfn, 'w') as fp:
+        fp.write('\t'.join(header[feattype]))
+        for line in lines:
             fp.write('\n')
-
-
+            fp.write('\t'.join([line[x] for x in linekeys[feattype]]))
