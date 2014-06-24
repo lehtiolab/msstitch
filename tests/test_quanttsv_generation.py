b3dba56d22b8f40e3feff1d87743ab47eb895d7b
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-06-24 13:26:23 +0200

    Removed openmz traces

--- tests/test_quanttsv_generation.py
+++ tests/test_quanttsv_generation.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch, Mock
-from app.preparation import openmz
+from app.preparation import quant as prep
 
 
 def mock_quantmaps(self):
@@ -29,21 +29,22 @@ class TestQuantDBLookups(unittest.TestCase):
 
     def test_get_quantheader(self):
         fn = 'test'
-        with patch('app.preparation.openmz.sqlite.QuantDB', self.mockdb):
-            gqh = openmz.get_quant_header
+        with patch('app.preparation.quant.sqlite.QuantDB', self.mockdb):
+            gqh = prep.get_quant_header
             assert gqh(fn) == ['116', '117', '118']
 
     def test_lookup_quant(self):
-        result = openmz.lookup_quant('fn', '1234', self.mockdb())
+        result = prep.lookup_quant('fn', '1234', self.mockdb())
         assert result == {'116': 1024,
                           '117': 4352,
                           '118': 4543,
                           }
 
     def test_generate_mzidtsv_quanted(self):
-        with patch('app.preparation.openmz.sqlite.QuantDB', self.mockdb), patch('app.preparation.openmz.readers', self.mockreader):
-            psms = openmz.generate_psms_quanted('fn', 'tsvfn',
-                                                list([x[0] for x in mock_quantmaps(self)]))
+        with patch('app.preparation.quant.sqlite.QuantDB', self.mockdb), patch('app.preparation.quant.readers', self.mockreader):
+            psms = prep.generate_psms_quanted('fn', 'tsvfn',
+                                              list([x[0] for x in
+                                                    mock_quantmaps(self)]))
             result = next(psms)
         line = next(mock_specscanlines('test'))[0] + [
             str(x[1]) for x in mock_findquants(self, 'test', '1234')]
@@ -54,16 +55,16 @@ class TestQuantDBLookups(unittest.TestCase):
 class TestQuantLines(unittest.TestCase):
     def test_convert_quantdict(self):
         qdata = {'116': 1024, '117': 2048}
-        allin = openmz.convert_quantdata_to_line(qdata, ['116', '117'])
+        allin = prep.convert_quantdata_to_line(qdata, ['116', '117'])
         self.assertEqual(allin, ['1024', '2048'])
-        notfound = openmz.convert_quantdata_to_line(qdata,
-                                                    ['116', '117', '118'])
+        notfound = prep.convert_quantdata_to_line(qdata,
+                                                  ['116', '117', '118'])
         self.assertEqual(notfound, ['1024', '2048', 'NA'])
 
     def create_tsv_header(self):
         qhead = list(mock_quantmaps(self))
-        with patch('app.preparation.openmz.readers.get_tsv_header',
+        with patch('app.preparation.quant.readers.get_tsv_header',
                    mock_tsv_header):
-            result = openmz.create_tsv_header_quant('test', qhead)
+            result = prep.create_tsv_header_quant('test', qhead)
         desired = next(mock_tsv_header()) + qhead
         self.assertEqual(result, desired)
