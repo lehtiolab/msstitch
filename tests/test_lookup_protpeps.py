fcacc93745a17115a833d83b3c61fe85e20ffc4f
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-09-17 09:37:14 +0000

    Fixed test bugs

--- tests/test_lookup_protpeps.py
+++ tests/test_lookup_protpeps.py
@@ -6,10 +6,13 @@ from app.lookups import protein_peptides as lookup
 class TestCreateLookup(unittest.TestCase):
     def setUp(self):
         self.mockdb = Mock
+        self.mockdb.create_ppdb = Mock()
+        self.mockdb.store_peptides_proteins = Mock()
+        self.mockdb.index = Mock()
 
     def test_calls_sqlite(self):
-        with patch(lookup.tsvreader.get_mzidtsv_lines_scannr_specfn,
-                   return_value=[]), patch(lookup.sqlite.ProteinPeptideDB,
+        with patch('app.lookups.protein_peptides.tsvreader.get_mzidtsv_lines_scannr_specfn',
+                   return_value=[]), patch('app.lookups.protein_peptides.sqlite.ProteinPeptideDB',
                                            self.mockdb):
             lookup.create_protein_pep_lookup('testfn')
         self.mockdb.create_ppdb.assert_called_with()
