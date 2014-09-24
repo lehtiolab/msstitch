706e37f48a4ca4ec6657f772e42b36c36fc0c1ec
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-09-24 13:44:25 +0000

    Testing protein group ranking

--- tests/integration_tests/proteingrouping.py
+++ tests/integration_tests/proteingrouping.py
@@ -15,12 +15,13 @@ class ProteinGroupTest(basetests.MzidTSVBaseTest):
 
     def get_proteingroups_per_peptide(self):
         with open(self.resultfn) as fp:
-            header = next(fp)
+            header = next(fp).strip().split('\t')
             pep_ix = header.index(constants.HEADER_PEPTIDE)
             master_ix = header.index(constants.HEADER_MASTER_PROT)
             pgcontent_ix = header.index(constants.HEADER_PG_CONTENT)
             pgamount_ix = header.index(constants.HEADER_PG_AMOUNT_PROTEIN_HITS)
             for line in fp:
+                line = line.strip().split('\t')
                 print(line)
                 yield {'peptide': line[pep_ix],
                        'master': line[master_ix],
@@ -32,28 +33,29 @@ class ProteinGroupTest(basetests.MzidTSVBaseTest):
         pgkeys = ['peptide', 'master', 'content', 'amount']
         for line in expected:
             exp = {}
-            for key, value in zip(pgkeys, line[:-1]):
+            for key, value in zip(pgkeys, line):
                 exp[key] = value
-            if unrolled:
-                exp['unroll'] = line[-1]
-            else:
-                exp['unroll'] = 1
-            for pepline in range(exp['unroll']):
-                res = next(result)
-                print(res)
-                for key in pgkeys:
-                    self.assertEqual(res[key], exp[key])
+            res = next(result)
+            print(res)
+            print(exp)
+            self.assertEqual(res['peptide'], exp['peptide'])
+            self.assertEqual(set(res['master'].split(';')),
+                             set(exp['master'].split(';')))
+            self.assertEqual(res['amount'], exp['amount'])
+            rescontent = res['content'].split(';')
+            expcontent = exp['content'].split(';')
+            self.assertEqual(set(rescontent), set(expcontent))
 
 
 class TestProteinGroupingConfidenceLevel(ProteinGroupTest):
-    expected = [['ABCD', 'PB', 'PA,PB,PC', '2', 2],
-                ['ABCD', 'PB', 'PA,PB,PC', '2', 2],
-                ['HIJKL', 'PB;PD', 'PA,PB,PC;PC,PD,PE', '2;2', 3],
-                ['MNOP', 'PD', 'PC,PD,PE', '2', 1],
-                ['QRST', 'PD;PF', 'PC,PD,PE;PE,PF,PG', '2,2', 3],
-                ['UVWX', 'PF', 'PE,PF,PG', '2', 2],
-                ['YZAB', 'PF', 'PE,PF,PG', '2', 2],
-                ['CDEF', 'PH', 'PH', '1', 1],
+    expected = [['ABCD', 'PB', 'PB,PA,PC', '2'],
+                ['ABCD', 'PB', 'PB,PA,PC', '2'],
+                ['HIJKL', 'PB;PD', 'PB,PA,PC;PD,PE,PC', '2;2'],
+                ['MNOP', 'PD', 'PD,PE,PC', '2'],
+                ['QRST', 'PD;PF', 'PD,PE,PC;PF,PG,PE', '2,2'],
+                ['UVWX', 'PF', 'PF,PG,PE', '2'],
+                ['YZAB', 'PF', 'PF,PG,PE', '2'],
+                ['CDEF', 'PH', 'PH', '1'],
                 ]
 
     def test_conflevel_higher_better(self):
@@ -68,6 +70,24 @@ class TestProteinGroupingConfidenceLevel(ProteinGroupTest):
 
 
 class TestProteinGroupingUnrolled(ProteinGroupTest):
+    expected = [['K.ABCD.S', 'PB', 'PB,PA,PC', '2'],
+                ['K.ABCD.S', 'PB', 'PB,PA,PC', '2'],
+                ['ABCD', 'PB', 'PB,PA,PC', '2'],
+                ['ABCD', 'PB', 'PB,PA,PC', '2'],
+                ['HIJKL', 'PB;PD', 'PB,PA,PC;PD,PE,PC', '2;2'],
+                ['HIJKL', 'PB;PD', 'PB,PA,PC;PD,PE,PC', '2;2'],
+                ['HIJKL', 'PB;PD', 'PB,PA,PC;PD,PE,PC', '2;2'],
+                ['.MNOP.', 'PD', 'PD,PE,PC', '2'],
+                ['QRST', 'PD;PF', 'PD,PE,PC;PF,PG,PE', '2,2'],
+                ['QRST', 'PD;PF', 'PD,PE,PC;PF,PG,PE', '2,2'],
+                ['QRST', 'PD;PF', 'PD,PE,PC;PF,PG,PE', '2,2'],
+                ['UVWX', 'PF', 'PF,PG,PE', '2'],
+                ['UVWX', 'PF', 'PF,PG,PE', '2'],
+                ['R.YZAB.L', 'PF', 'PF,PG,PE', '2'],
+                ['R.YZAB.G', 'PF', 'PF,PG,PE', '2'],
+                ['CDEF', 'PH', 'PH', '1'],
+                ]
+
     infilename = 'mzidtsv_unrolled.txt'
 
     def test_unrolled(self):
