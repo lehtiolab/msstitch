794d7dd4a136bd4e0b4fe13259342972a77d4010
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2014-12-03 15:03:16 +0100

    Testing prottable add protein data

--- app/preparation/prottable.py
+++ app/preparation/prottable.py
@@ -19,33 +19,40 @@ def get_header_with_proteindata(header):
 
 def add_protein_data(proteins, header, pgdb):
     for protein in proteins:
+        outprotein = {k: v for k, v in protein.items()}
         protein_acc = protein[prottabledata.HEADER_PROTEIN]
-        #protein data is ((psm_id, psmseq, fakemaster, all_group_proteins_acc,
-        #                   coverage, description),)
-        protein_data = pgdb.get_protein_data(protein_acc)
-        description = protein_data[0][5]
-        coverage = protein_data[0][4]
-        psmcount = len(set([x[0] for x in protein_data]))
-        pepcount = len(set([x[1] for x in protein_data]))
-        proteincount = len(set([x[3] for x in protein_data]))
-        peptides_master_map = {}
-        for psm in protein_data:
-            try:
-                peptides_master_map[psm[1]].add(psm[2])
-            except KeyError:
-                peptides_master_map[psm[1]] = {psm[2]}
-        unipepcount = len([x for x in peptides_master_map
-                           if len(peptides_master_map[x]) == 1])
-        return {prottabledata.HEADER_DESCRIPTION: description,
-                prottabledata.HEADER_COVERAGE: coverage,
-                prottabledata.HEADER_NO_PROTEIN: proteincount,
-                prottabledata.HEADER_NO_UNIPEP: unipepcount,
-                prottabledata.HEADER_NO_PEPTIDE: pepcount,
-                prottabledata.HEADER_NO_PSM: psmcount,
-                #prottabledata.HEADER_AREA: area,
-                #prottabledata.HEADER_NO_QUANT_PSM: quantcount,
-                #prottabledata.HEADER_CV_QUANT_PSM: quantcv,
-                }
+        outprotein.update(get_protein_data(protein_acc, pgdb))
+        outprotein = {k: str(v) for k, v in outprotein.items()}
+        yield outprotein
+
+
+def get_protein_data(protein_acc, pgdb):
+    #protein data is ((psm_id, psmseq, fakemaster, all_group_proteins_acc,
+    #                   coverage, description),)
+    protein_data = pgdb.get_protein_data(protein_acc)
+    description = protein_data[0][5]
+    coverage = protein_data[0][4]
+    psmcount = len(set([x[0] for x in protein_data]))
+    pepcount = len(set([x[1] for x in protein_data]))
+    proteincount = len(set([x[3] for x in protein_data]))
+    peptides_master_map = {}
+    for psm in protein_data:
+        try:
+            peptides_master_map[psm[1]].add(psm[2])
+        except KeyError:
+            peptides_master_map[psm[1]] = {psm[2]}
+    unipepcount = len([x for x in peptides_master_map
+                       if len(peptides_master_map[x]) == 1])
+    return {prottabledata.HEADER_DESCRIPTION: description,
+            prottabledata.HEADER_COVERAGE: coverage,
+            prottabledata.HEADER_NO_PROTEIN: proteincount,
+            prottabledata.HEADER_NO_UNIPEP: unipepcount,
+            prottabledata.HEADER_NO_PEPTIDE: pepcount,
+            prottabledata.HEADER_NO_PSM: psmcount,
+            #prottabledata.HEADER_AREA: area,
+            #prottabledata.HEADER_NO_QUANT_PSM: quantcount,
+            #prottabledata.HEADER_CV_QUANT_PSM: quantcv,
+            }
 
 
 def collect_descriptions(pgdb, fastafn):
