daeb64be14836e7295f8330fd364fa3ffd7860e3
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-02-13 15:31:52 +0100

    Changed sqlite model for quant lookups. Skewing towards letting the db do the work

--- app/lookups/quant.py
+++ app/lookups/quant.py
@@ -1,54 +1,69 @@
 from decimal import Decimal, getcontext
 
 from app.lookups.sqlite import quant as sqlite
-from app import formatting
 from app.readers import openms as openmsreader
 from app.readers import spectra as specreader
 
 
-def create_quant_lookup(fn_spectra, workdir, consensus_els):
+def initiate_quant_lookup(workdir):
+    """Creates a quant db sqlite file and fills a table with spectra mzml
+    info on scan nrs and retention times"""
+    quantdb = sqlite.QuantDB()
+    quantdb.create_quantdb(workdir)
+    return quantdb
+
+
+def get_quant_lookup(quantfn):
+    return sqlite.QuantDB(quantfn)
+
+
+def create_isobaric_quant_lookup(quantdb, specfn_consensus_els):
     """Creates an sqlite lookup table of scannrs with quant data.
 
     spectra - an iterable of tupled (filename, spectra)
     consensus_els - a iterable with consensusElements"""
-    quantdb = sqlite.QuantDB()
-    quantdb.create_quantdb(workdir)
-    quants = {}
-    count = 0
-    for consensus_el in consensus_els:
-        count += 1
+    quants = []
+    for specfn, consensus_el in specfn_consensus_els:
         rt = openmsreader.get_consxml_rt(consensus_el)
-        fn, spec_scan_nr = get_spec_scan_nr(fn_spectra, rt)
         qdata = get_quant_data(consensus_el)
-        try:
-            quants[fn][spec_scan_nr] = qdata
-        except KeyError:
-            quants[fn] = {}
-            quants[fn][spec_scan_nr] = qdata
-        if count == 5000:
-            store_quants(quants, quantdb)
-            count = 0
-            quants = {}
-        formatting.clear_el(consensus_el)
-    store_quants(quants, quantdb)
-    quantdb.index_quants()
-    return quantdb.fn
+        for quantmap in sorted(qdata.keys()):
+            quants.append((specfn, rt, quantmap, qdata[quantmap]))
+            if len(quants) == 5000:
+                quantdb.store_isobaric_quants(quants)
+    quantdb.store_isobaric_quants(quants)
+    quantdb.index_isobaric_quants()
 
 
-def get_spec_scan_nr(fn_spectra, cons_rt):
-    """Returns mzML filename and scan nr of a spectrum that belongs to the
-    retention time in cons_rt, when given a generator of multiple
-    spectra files in fn_spectra.
-    The generator is a tuple of fn, spectrum, namespace."""
-    getcontext().prec = 14 # sets decimal point precision
-    cons_rt_dec = Decimal(cons_rt)
+def create_precursor_quant_lookup(quantdb, mzmlfn_featsxml):
+    """Fills quant sqlite with precursor quant from:
+        features - generator of xml features from openms
+    """
+    features = []
+    getcontext().prec = 14  # sets decimal point precision
+    for specfn, feat_element in mzmlfn_featsxml:
+        feat = openmsreader.get_feature_info(feat_element)
+        feat['rt'] = float(Decimal(feat['rt']))
+        features.append((specfn, feat['rt'], feat['mz'],
+                         feat['charge'], feat['intensity'])
+                        )
+        if len(features) == 5000:
+            quantdb.store_ms1_quants(features)
+            features = []
+    quantdb.store_ms1_quants(features)
+    quantdb.index_precursor_quants()
+
+
+def create_spectra_lookup(quantdb, fn_spectra):
+    """Stores all spectra rt and scan nr in db"""
+    to_store = []
     for fn, spectrum, ns in fn_spectra:
         mzml_rt = Decimal(specreader.get_mzml_rt(spectrum, ns))
-        if float(cons_rt_dec / 60) == float(mzml_rt):
-            scan_nr = specreader.get_spec_scan_nr(spectrum)
-            return fn, scan_nr
-        formatting.clear_el(spectrum)
-    return False
+        scan_nr = specreader.get_spec_scan_nr(spectrum)
+        to_store.append((fn, scan_nr, mzml_rt))
+        if len(to_store) == 5000:
+            quantdb.store_mzmls(to_store)
+            to_store = []
+    quantdb.store_mzmls(to_store)
 
 
 def get_quant_data(cons_el):
@@ -57,13 +72,3 @@ def get_quant_data(cons_el):
     for reporter in cons_el.findall('.//element'):
         quant_out[reporter.attrib['map']] = reporter.attrib['it']
     return quant_out
-
-
-def store_quants(quants, quantdb):
-    sql_quants = []
-    for specfn in quants:
-        for scannr, quantdata in quants[specfn].items():
-            for quantmap in sorted(quantdata.keys()):
-                sql_quants.append((specfn, scannr,
-                                   quantmap, quantdata[quantmap]))
-    quantdb.store_quants(sql_quants)
--- app/lookups/sqlite/quant.py
+++ app/lookups/sqlite/quant.py
@@ -4,30 +4,113 @@ from app.lookups.sqlite.base import DatabaseConnection
 class QuantDB(DatabaseConnection):
     def create_quantdb(self, workdir):
         self.create_db(workdir,
-                       {'quant': ['spectra_filename TEXT', 'scan_nr TEXT',
-                                  'quantmap TEXT', 'intensity REAL']})
+                       {'mzml': ['mzmlfilename TEXT',
+                                 'scan_nr TEXT',
+                                 'retention_time REAL'],
+                        # FIXME in future, make possible to build full db in
+                        # small steps. This will speed up pipeline since
+                        # identification takes longer time than quanting. Also
+                        # would be good to make one large sqlite file with id,
+                        # protein group, quant in it. Problem is: ID is
+                        # slowest, but thats where the foreign keys will refer
+                        # to, if we dont do a RT/MS2/file db from the mzml
+                        # FIXME all iso/precursor quants, or only ones with IDs?
+                        # if only ones with ids, we can foreign key to ID
+                        # table.
+                        # FIXME isoquant should get retention time instead of
+                        # scan nr so we can fk join on rt/specfile to the
+                        # mzml table
+                        # FIXME lookup of ms1 quant like this:
+                        # scannr -> rt from mzmltable; rt interval & mz
+                        # interval -> ms1quants -> select best match on mz
+                        'isobaric_quant': ['mzmlfilename TEXT'
+                                           'retention_time REAL',
+                                           'quantmap TEXT',
+                                           'intensity REAL',
+                                           'FOREIGN KEY(mzmlfilename)',
+                                           'REFERENCES mzml '
+                                           'USING(mzmlfilename)'
+                                           'FOREIGN KEY(retention_time)',
+                                           'REFERENCES mzml '
+                                           'USING(retention_time)'
+                                           ],
+                        'ms1_quant': ['mzmlfilename TEXT',
+                                      'retention_time REAL', 'mz REAL',
+                                      'charge INTEGER', 'intensity REAL',
+                                      'FOREIGN KEY(mzmlfilename)',
+                                      'REFERENCES mzml '
+                                      'USING(mzmlfilename)'
+                                      'FOREIGN KEY(retention_time)',
+                                      'REFERENCES mzml '
+                                      'USING(retention_time)'
+                                      ]})
 
-    def store_quants(self, quants):
+    def store_isobaric_quants(self, quants):
+        self.store_many(
+            'INSERT INTO isobaric_quant(mzmlfilename, retention_time, '
+            'quantmap, intensity) VALUES (?, ?, ?, ?)', quants)
+
+    def store_mzmls(self, spectra):
+        self.store_many(
+            'INSERT INTO mzml(mzmlfilename, scan_nr, retention_time) '
+            'VALUES (?, ?, ?)', spectra)
+
+    def store_ms1_quants(self, quants):
+        self.store_many(
+            'INSERT INTO ms1_quant(mzmlfilename, retention_time, mz, '
+            'charge, intensity) VALUES (?, ?, ?, ?, ?)', quants)
+
+    def store_many(self, sql, values):
         cursor = self.get_cursor()
-        cursor.executemany(
-            'INSERT INTO quant(spectra_filename, scan_nr, quantmap, intensity)'
-            ' VALUES (?, ?, ?, ?)',
-            quants)
+        cursor.executemany(sql, values)
         self.conn.commit()
 
-    def index_quants(self):
-        self.index_column('spec_index', 'quant', 'spectra_filename')
-        self.index_column('scan_index', 'quant', 'scan_nr')
+    def index_mzml(self):
+        self.index_column('mzmlfn_index', 'mzml', 'mzmlfilename')
+        self.index_column('scan_index', 'mzml', 'scan_nr')
+        self.index_column('rt_index', 'mzml', 'retention_time')
 
-    def lookup_quant(self, spectrafile, scannr):
+    def index_isobaric_quants(self):
+        self.index_column('mzmlfn_index', 'isobaric_quant', 'mzmlfilename')
+        self.index_column('rt_index', 'isobaric_quant', 'retention_time')
+
+    def index_precursor_quants(self):
+        self.index_column('mzmlfn_index', 'isobaric_quant', 'mzmlfilename')
+        self.index_column('rt_index', 'isobaric_quant', 'retention_time')
+        self.index_column('charge_index', 'isobaric_quant', 'charge')
+        self.index_column('mz_index', 'isobaric_quant', 'mz')
+
+    def lookup_retention_time(self, spectrafile, scannr):
         cursor = self.get_cursor()
         cursor.execute(
-            'SELECT quantmap, intensity FROM quant WHERE '
-            'spectra_filename=? AND scan_nr=?', (spectrafile, scannr))
+            'SELECT retention_time FROM mzml '
+            'WHERE spectra_filename=? AND scan_nr=?',
+            (spectrafile, scannr))
         return cursor.fetchall()
+
+    def lookup_isobaric_quant(self, spectrafile, scannr):
+        cursor = self.get_cursor()
+        cursor.execute(
+            'SELECT iq.quantmap, iq.intensity '
+            'FROM mzml AS mz '
+            'JOIN isobaric_quant AS iq USING(retention_time) '
+            'WHERE spectra_filename=? AND scan_nr=?', (spectrafile, scannr))
+        return cursor.fetchall()
+
+    def lookup_precursor_quant(self, spectrafile, charge, minrt, maxrt,
+                               minmz, maxmz):
+        # FIXME check if this is slow since it has two BETWEEN in it
+        # we could replace the mz BETWEEN by filtering in python
+        cursor = self.get_cursor()
+        return cursor.execute(
+            'SELECT retention_time, mz, intensity '
+            'FROM precur_quants '
+            'WHERE mzmlfilename=? AND charge=? AND '
+            'rt BETWEEN ? AND ? AND mz BETWEEN ? AND ?',
+            (spectrafile, charge, minrt, maxrt, minmz, maxmz))
 
     def get_all_quantmaps(self):
         cursor = self.get_cursor()
         cursor.execute(
-            'SELECT DISTINCT quantmap FROM quant')
+            'SELECT DISTINCT quantmap FROM isobaric_quant')
         return cursor.fetchall()
