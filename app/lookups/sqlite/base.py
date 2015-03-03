95eb48c9a75d618731b7573115e3e3cf3f4f9b1e
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-03-03 15:49:40 +0100

    Started redesigning mslookup sql by uniting db schemas for the different results

--- app/lookups/sqlite/base.py
+++ app/lookups/sqlite/base.py
@@ -3,6 +3,99 @@ import os
 from tempfile import mkstemp
 
 
+mslookup_tables = {'mzml': ['spectra_id INTEGER PRIMARY KEY',
+                            'mzmlfile_id INTEGER',
+                            'scan_nr TEXT',
+                            'retention_time REAL',
+                            'FOREIGN KEY(mzmlfile_id)'
+                            'REFERENCES mzmlfiles'],
+                   'biosets': ['set_id INTEGER PRIMARY KEY',
+                               'set_name TEXT'],
+                   'mzmlfiles': ['mzmlfile_id INTEGER PRIMARY KEY',
+                                 'mzmlfilename TEXT',
+                                 'set_id INTEGER',
+                                 'FOREIGN KEY(set_id)'
+                                 'REFERENCES biosets'
+                                 ],
+                   'isobaric_quant': ['mzmlfile_id INTEGER',
+                                      'retention_time REAL',
+                                      'quantmap TEXT',
+                                      'intensity REAL',
+                                      'FOREIGN KEY(mzmlfile_id)'
+                                      'REFERENCES mzmlfiles '
+                                      'FOREIGN KEY(retention_time)'
+                                      'REFERENCES mzml'
+                                      ],
+                   'ms1_quant': ['mzmlfile_id INTEGER',
+                                 'retention_time REAL', 'mz REAL',
+                                 'charge INTEGER', 'intensity REAL',
+                                 'FOREIGN KEY(mzmlfile_id)'
+                                 'REFERENCES mzmlfiles '
+                                 'FOREIGN KEY(retention_time)'
+                                 'REFERENCES mzml'],
+                   'psms': ['psm_id TEXT PRIMARY KEY NOT NULL',
+                            'sequence TEXT',
+                            'score TEXT',
+                            'set_name TEXT',
+                            'spectra_id INTEGER',
+                            'FOREIGN KEY(spectra_id)'
+                            'REFERENCES mzml'
+                            ],
+                   # FIXME create a spectra_id: scan nr reference when
+                   # inserting psms, otherwise we must look them up?
+                   # protein table - protein ref, quant data, set ref
+                   'psmrows': ['psm_id TEXT',
+                               'rownr INTEGER',
+                               'FOREIGN KEY(psm_id) '
+                               'REFERENCES psms(psm_id)'],
+                   'proteins': ['protein_acc TEXT PRIMARY KEY NOT NULL'],
+                   'protein_psm': ['protein_acc TEXT',
+                                   'psm_id TEXT',
+                                   'FOREIGN KEY(protein_acc) '
+                                   'REFERENCES proteins(protein_acc) '
+                                   'FOREIGN KEY(psm_id) '
+                                   'REFERENCES psms(psm_id)'],
+                   'protein_evidence': ['protein_acc TEXT',
+                                        'evidence_lvl REAL',
+                                        'FOREIGN KEY(protein_acc) '
+                                        'REFERENCES '
+                                        'proteins(protein_acc)'],
+                   'protein_seq': ['protein_acc TEXT',
+                                   'sequence TEXT',
+                                   'FOREIGN KEY(protein_acc) '
+                                   'REFERENCES '
+                                   'proteins(protein_acc)'],
+                   'protein_coverage': ['protein_acc TEXT',
+                                        'coverage REAL',
+                                        'FOREIGN KEY(protein_acc) '
+                                        'REFERENCES '
+                                        'proteins(protein_acc)'],
+                   'protein_group_master': ['protein_acc TEXT',
+                                            'FOREIGN KEY(protein_acc) '
+                                            'REFERENCES '
+                                            'proteins(protein_acc)'],
+                   'protein_group_content': ['protein_acc TEXT',
+                                             'master TEXT',
+                                             'peptide_count INTEGER',
+                                             'psm_count INTEGER',
+                                             'protein_score INTEGER',
+                                             'FOREIGN KEY(protein_acc) '
+                                             'REFERENCES '
+                                             'proteins(protein_acc) '
+                                             'FOREIGN KEY(master) '
+                                             'REFERENCES '
+                                             'proteins'
+                                             '(protein_acc)'
+                                             ],
+                   'psm_protein_groups': ['psm_id TEXT',
+                                          'master TEXT',
+                                          'FOREIGN KEY(psm_id) REFERENCES'
+                                          ' psms(psm_id)',
+                                          'FOREIGN KEY(master) REFERENCES'
+                                          ' proteins(protein_acc)'],
+                   }
+
+
 class DatabaseConnection(object):
     def __init__(self, fn=None, foreign_keys=False):
         self.fn = fn
@@ -14,7 +107,8 @@ class DatabaseConnection(object):
 
     def create_db(self, workdir, tables, outfn=None, foreign_keys=False):
         """Creates a sqlite db file.
-        tables is a dict with keys=table names, values=lists of cols.
+        tables is a list with table names, which will be looked up in
+        mslookup_tables.
         """
         if outfn is None:
             fd, outfn = mkstemp(prefix='msstitcher_tmp_', dir=workdir)
@@ -26,7 +120,7 @@ class DatabaseConnection(object):
     def create_tables(self, tables):
         cursor = self.get_cursor()
         for table in tables:
-            columns = tables[table]
+            columns = mslookup_tables[table]
             cursor.execute('CREATE TABLE {0}({1})'.format(
                 table, ', '.join(columns)))
         self.conn.commit()
