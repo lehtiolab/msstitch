2c1019acc5296a5c4523ff2fbd3f9f0b2abd584f
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-03-04 20:36:08 +0100

    Fixes on bioset storing code

--- app/actions/mslookup/biosets.py
+++ app/actions/mslookup/biosets.py
@@ -1,7 +1,7 @@
-def create_set_lookup(lookupdb, spectrafns, set_names):
-    lookupdb.store_setnames(((x,) for x in set_names))
+def create_bioset_lookup(lookupdb, spectrafns, set_names):
+    lookupdb.store_biosets(((x,) for x in set_names))
     set_id_map = lookupdb.get_setnames()
-    biosets = ((fn, set_id_map[setname]) for fn, setname in
-               zip(spectrafns, set_names))
-    lookupdb.store_biosets(biosets)
+    mzmlfiles = ((fn, set_id_map[setname]) for fn, setname in
+                 zip(spectrafns, set_names))
+    lookupdb.store_mzmlfiles(mzmlfiles)
     lookupdb.index_biosets()
--- app/lookups/sqlite/biosets.py
+++ app/lookups/sqlite/biosets.py
@@ -8,5 +8,10 @@ class BioSetDB(DatabaseConnection):
     def store_biosets(self, biosets):
         self.store_many('INSERT INTO biosets(set_name) VALUES (?)', biosets)
 
-    def index_precursor_quants(self):
+    def store_mzmlfiles(self, mzmlfiles):
+        self.store_many('INSERT INTO mzmlfiles(mzmlfilname, set_id) '
+                        'VALUES (?)', mzmlfiles)
+
+    def index_biosets(self):
         self.index_column('setname_index', 'biosets', 'set_name')
+        self.index_column('mzmlfn_index', 'mzmlfiles', 'mzmlfilename')
