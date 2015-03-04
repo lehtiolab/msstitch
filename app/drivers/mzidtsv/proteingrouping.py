7c1407405ca7895eca0d6b23d9e9a1afabb0167c
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-03-04 16:10:06 +0100

    Cleaned protein group sql driver from mzidtsv module

--- app/drivers/mzidtsv/proteingrouping.py
+++ app/drivers/mzidtsv/proteingrouping.py
@@ -1,7 +1,4 @@
-import shutil
-
 from app.preparation.mzidtsv import proteingrouping as prep
-from app.lookups import protein_peptide as lookups
 from app.drivers.mzidtsv import MzidTSVDriver
 from app.readers import fasta
 from app.lookups.sqlite.proteingroups import ProteinGroupProteinTableDB
@@ -32,30 +29,3 @@ class ProteinGroupDriver(MzidTSVDriver):
                                                           self.unroll,
                                                           self.coverage,
                                                           self.evidence_levels)
-
-
-class ProteinGroupLookupDriver(MzidTSVDriver):
-    outsuffix = '_protgrouplookup.txt'
-
-    def create_protein_pep_lookup(self, confkey):
-        return lookups.create_protein_pep_lookup(self.fn,
-                                                 self.oldheader,
-                                                 confkey,
-                                                 self.conflvl,
-                                                 self.lowerbetter,
-                                                 self.unroll,
-                                                 self.fasta,
-                                                 self.evidence_levels)
-
-    def get_psms(self):
-        # FIXME Should really be called process_psms, but have to change in
-        # all mzidtsv drivers
-        confkey = self.oldheader[int(self.confcol) - 1]
-        self.lookup = self.create_protein_pep_lookup(confkey)
-        prep.build_proteingroup_db(self.fn, self.oldheader,
-                                   self.lookup,
-                                   confkey, self.conflvl, self.lowerbetter,
-                                   self.unroll, self.coverage)
-
-    def write(self):
-        pass
