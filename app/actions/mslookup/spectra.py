c3656152b39ea8c8199d1dd51b165e4f7b753a12
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-03-04 20:54:01 +0100

    Fix spectra lookup building

--- app/actions/mslookup/spectra.py
+++ app/actions/mslookup/spectra.py
@@ -3,16 +3,17 @@ from decimal import Decimal, getcontext
 from app.readers import spectra as specreader
 
 
-def create_spectra_lookup(quantdb, fn_spectra):
+def create_spectra_lookup(lookup, fn_spectra):
     """Stores all spectra rt and scan nr in db"""
     getcontext().prec = 14  # sets decimal point precision
     to_store = []
+    mzmlmap = {fn: fnid for fn, fnid in lookup.get_mzmlfile_map()}
     for fn, spectrum, ns in fn_spectra:
         mzml_rt = float(Decimal(specreader.get_mzml_rt(spectrum, ns)))
         scan_nr = specreader.get_spec_scan_nr(spectrum)
-        to_store.append((fn, scan_nr, mzml_rt))
+        to_store.append((mzmlmap[fn], scan_nr, mzml_rt))
         if len(to_store) == 5000:
-            quantdb.store_mzmls(to_store)
+            lookup.store_mzmls(to_store)
             to_store = []
-    quantdb.store_mzmls(to_store)
-    quantdb.index_mzml()
+    lookup.store_mzmls(to_store)
+    lookup.index_mzml()
--- app/lookups/sqlite/spectra.py
+++ app/lookups/sqlite/spectra.py
@@ -5,12 +5,17 @@ class SpectraDB(DatabaseConnection):
     def add_tables(self):
         self.create_tables(['mzml'])
 
+    def get_mzmlfile_map(self):
+        cursor = self.get_cursor()
+        cursor.execute('SELECT mzmlfile_id, mzmlfilename FROM mzmlfiles')
+        return cursor.fetchall()
+
     def store_mzmls(self, spectra):
         self.store_many(
-            'INSERT INTO mzml(mzmlfilename, scan_nr, retention_time) '
+            'INSERT INTO mzml(mzmlfile_id, scan_nr, retention_time) '
             'VALUES (?, ?, ?)', spectra)
 
     def index_mzml(self):
-        self.index_column('mzmlfn_index', 'mzml', 'mzmlfilename')
+        self.index_column('mzmlfnid_index', 'mzml', 'mzmlfile_id')
         self.index_column('scan_index', 'mzml', 'scan_nr')
         self.index_column('rt_index', 'mzml', 'retention_time')
