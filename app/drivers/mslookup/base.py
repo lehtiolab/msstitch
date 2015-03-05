8f65fdfdfb6413485c765916b3885583db9bd0de
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-03-05 18:00:22 +0100

    Spectra and biosets can be created at the same time

--- app/drivers/mslookup/base.py
+++ app/drivers/mslookup/base.py
@@ -8,7 +8,6 @@ class LookupDriver(BaseDriver):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.lookupfn = kwargs.get('lookup', None)
-        self.initialize_lookup()
 
     def initialize_lookup(self):
         if self.lookupfn is not None:
@@ -18,8 +17,10 @@ class LookupDriver(BaseDriver):
             # correct lookuptype!
             self.lookupfn = os.path.join(self.outdir,
                                          'msstitcher_lookup.sqlite')
-            self.lookup = lookups.create_new_lookup(self.lookupfn, self.lookuptype)
+            self.lookup = lookups.create_new_lookup(self.lookupfn,
+                                                    self.lookuptype)
         self.lookup.add_tables()
 
     def run(self):
+        self.initialize_lookup()
         self.create_lookup()
--- app/drivers/mslookup/spectra.py
+++ app/drivers/mslookup/spectra.py
@@ -10,7 +10,14 @@ class SpectraLookupDriver(base.LookupDriver):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.spectrafns = self.fn
+        self.setnames = kwargs.get('setnames', None)
+        if self.setnames is None:
+            assert self.lookup is not None, ('Must specify lookup '
+                                             'if setnames have not '
+                                             'been provided')
 
     def create_lookup(self):
+        lookups.create_bioset_lookup(self.lookup, self.spectrafns,
+                                     self.setnames)
         fn_spectra = spectrareader.mzmlfn_spectra_generator(self.spectrafns)
         lookups.create_spectra_lookup(self.lookup, fn_spectra)
