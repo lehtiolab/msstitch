5ee2390b0994ed0aefb8f2fcd69ce3a1900c6af0
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-03-19 16:49:45 +0100

    Added sqlite protein quant module

---
+++ app/lookups/sqlite/proteinquant.py
@@ -0,0 +1,18 @@
+from app.lookups.sqlite.base import ResultLookupInterface
+
+
+class ProtQuantDB(ResultLookupInterface):
+    def add_tables(self):
+        self.create_tables(['protein_quanted', 'protquant_channels',
+                            'protein_quanted_psms'])
+
+    def store_quant_channels(self, quantchannels):
+        self.store_many(
+            'INSERT INTO protquant_channels(channel_name) VALUES (?)',
+            quantchannels)
+
+    def get_quantchannel_ids(self):
+        cursor = self.get_cursor()
+        cursor.execute(
+            'SELECT channel_id, channel_name FROM protquant_channels')
+        return {channel: chan_id for chan_id, channel in cursor}
