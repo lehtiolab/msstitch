4512eef66c8c6e07f5a47d377c26afa21b252feb
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-06-21 10:28:27 +0200

    MS1 quant for proteins is from PSMs, not peptides. Highest PSMs (that made FDR cut) are taken, not highest peptides (which are PSMs with best svm score)

--- app/actions/prottable.py
+++ app/actions/prottable.py
@@ -3,31 +3,31 @@ from app.dataformats import prottable as prottabledata
 from app.dataformats import mzidtsv as mzidtsvdata
 
 
-def add_ms1_quant_from_top3_mzidtsv(proteins, peptides):
-    """Collects peptides or PSMs with the highes precursor quant values,
+def add_ms1_quant_from_top3_mzidtsv(proteins, psms):
+    """Collects PSMs with the highes precursor quant values,
     adds sum of the top 3 of these to a protein table"""
-    top_ms1_peptides = {}
-    for peptide in peptides:
-        protacc = peptide[mzidtsvdata.HEADER_MASTER_PROT]
-        precursor_amount = peptide[mzidtsvdata.HEADER_PRECURSOR_QUANT]
+    top_ms1_psms = {}
+    for psm in psms:
+        protacc = psm[mzidtsvdata.HEADER_MASTER_PROT]
+        precursor_amount = psm[mzidtsvdata.HEADER_PRECURSOR_QUANT]
         if ';' in protacc or precursor_amount == 'NA':
             continue
         precursor_amount = float(precursor_amount)
-        pep_id = tsvreader.get_psm_id(peptide)
+        psm_seq = psm[mzidtsvdata.HEADER_PEPTIDE]
         try:
-            min_precursor_amount = min(top_ms1_peptides[protacc])
+            min_precursor_amount = min(top_ms1_psms[protacc])
         except KeyError:
-            top_ms1_peptides[protacc] = {-1: None, -2: None,
-                                         precursor_amount: pep_id}
+            top_ms1_psms[protacc] = {-1: None, -2: None,
+                                         precursor_amount: psm_seq}
             continue
         else:
             if precursor_amount > min_precursor_amount:
-                top_ms1_peptides[protacc][precursor_amount] = pep_id
-                top_ms1_peptides[protacc].pop(min_precursor_amount)
+                top_ms1_psms[protacc][precursor_amount] = psm_seq 
+                top_ms1_psms[protacc].pop(min_precursor_amount)
     for protein in proteins:
         outprotein = {k: v for k, v in protein.items()}
         try:
-            amounts = top_ms1_peptides[protein[prottabledata.HEADER_PROTEIN]]
+            amounts = top_ms1_psms[protein[prottabledata.HEADER_PROTEIN]]
         except KeyError:
             prec_area = 'NA'
         else:
