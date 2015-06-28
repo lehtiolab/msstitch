eccbe566d04e01f50850bc195b1124cd914f2ede
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-06-28 17:40:51 +0200

    Removed modifying header fields with splitpool name for mzidtsv splittsv. This information is useless since split pool is in filename and usually also in a column

--- app/writers/mzidtsv.py
+++ app/writers/mzidtsv.py
@@ -5,7 +5,7 @@ def write_mzid_tsv(header, psms, outfn):
     tsv.write_tsv(header, psms, outfn)
 
 
-def write_multi_mzidtsv(baseheader, oldheader, psms, base_outfile):
+def write_multi_mzidtsv(header, oldheader, psms, base_outfile):
     outfile_handles = {}
     for psm in psms:
         line = psm['psm']
@@ -13,13 +13,6 @@ def write_multi_mzidtsv(baseheader, oldheader, psms, base_outfile):
         if split_pool not in outfile_handles:
             outfile_handles[split_pool] = open(base_outfile.format(split_pool),
                                                'w')
-            header = format_split_header(baseheader, split_pool)
             tsv.write_tsv_line_from_list(header, outfile_handles[split_pool])
         tsv.write_tsv_line_from_list([line[field] for field in oldheader],
                                      outfile_handles[split_pool])
-
-
-def format_split_header(baseheader, split_pool):
-    """Returns header with split pool name in header fields where
-    applicable."""
-    return [x.format(split_pool) for x in baseheader]
