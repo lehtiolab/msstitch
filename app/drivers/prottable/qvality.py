084235790c466a172ee1e07c39f70fae848aecca
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-07-05 21:55:34 +0200

    Not forget to make qvality understand that lower scores are better!

--- app/drivers/prottable/qvality.py
+++ app/drivers/prottable/qvality.py
@@ -7,6 +7,8 @@ class ProttableQvalityDriver(QvalityDriver):
     def __init__(self, **kwargs):
         super(ProttableQvalityDriver).__init__(**kwargs)
         self.score_get_fun = preparation.prepare_qvality_input
+        if '***reverse' not in self.qvalityoptions:
+            self.qvalityoptions.extend(['***reverse'])
 
     def set_features(self):
         targetheader = tsv.get_tsv_header(self.fn)
