8211f3055902711e82c8b4d5caafced8445530e7
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-08-31 17:14:05 +0200

    Had to change operation order to match evidence to protein acc nrs since they are not primary key anymore

--- app/lookups/sqlite/proteingroups.py
+++ app/lookups/sqlite/proteingroups.py
@@ -25,6 +25,9 @@ class ProteinGroupDB(ResultLookupInterface):
         cursor.executemany(
             'INSERT INTO proteins(protein_acc) '
             'VALUES(?)', proteins)
+        self.conn.commit()
+        self.index_column('proteins_index', 'proteins', 'protein_acc')
+        cursor = self.get_cursor()
         cursor.executemany(
             'INSERT INTO protein_evidence(protein_acc, evidence_lvl) '
             'VALUES(?, ?)', evidence_lvls)
@@ -33,7 +36,6 @@ class ProteinGroupDB(ResultLookupInterface):
                 'INSERT INTO protein_seq(protein_acc, sequence) '
                 'VALUES(?, ?)', sequences)
         self.conn.commit()
-        self.index_column('proteins_index', 'proteins', 'protein_acc')
 
     def store_descriptions(self, descriptions):
         cursor = self.get_cursor()
