1e788122ff215f6dc7326cb12395ea0984683920
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-09-16 16:35:36 +0200

    Fixing add isoquant data to prottable

--- app/actions/prottable/isoquant.py
+++ app/actions/prottable/isoquant.py
@@ -2,15 +2,18 @@ from app.dataformats import prottable as prottabledata
 from app.readers import tsv as reader
 
 
-def add_isoquant_data(proteins, quantproteins, quantpattern, quantheader):
+def add_isoquant_data(proteins, quantproteins, quantfields):
     """Runs through a protein table and adds quant data from ANOTHER protein
     table that contains that data."""
-    quantfields = reader.get_cols_in_file(quantpattern, quantheader)
     quant_proteinmap = get_quantprotein_map(quantproteins, quantfields)
     for protein in proteins:
         prot_acc = protein[prottabledata.HEADER_PROTEIN]
         outprotein = {k: v for k, v in protein.items()}
-        outprotein.update({k: v for k, v in quant_proteinmap[prot_acc]})
+        try:
+            outprotein.update(quant_proteinmap[prot_acc])
+        except KeyError:
+            outprotein.update({field: 'NA' for field in quantfields})
+        print(outprotein)
         yield outprotein
 
 
@@ -21,5 +24,5 @@ def get_quantprotein_map(proteins, quantfields):
     qmap = {}
     for protein in proteins:
         prot_acc = protein.pop(prottabledata.HEADER_PROTEIN)
-        qmap[prot_acc].update({qf: protein[qf] for qf in quantfields})
+        qmap[prot_acc] = {qf: protein[qf] for qf in quantfields}
     return qmap
--- app/drivers/prottable/isoquant.py
+++ app/drivers/prottable/isoquant.py
@@ -9,16 +9,20 @@ class AddIsobaricQuantDriver(ProttableAddData):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.quantfile = kwargs.get('quantfile', False)
+        self.quantpattern = kwargs.get('isobquantcolpattern', False)
         self.headertypes = ['isoquant']
 
     def initialize_input(self):
         super().initialize_input()
-        self.quantheader = reader.get_tsv_header(self.quantfile)
+        quantheader = reader.get_tsv_header(self.quantfile)
+        self.quantfields = reader.get_cols_in_file(self.quantpattern, quantheader)
         self.quantproteins = reader.generate_tsv_proteins(self.quantfile,
-                                                          self.quantheader)
+                                                          quantheader)
+
+    def create_header(self):
+        self.header = self.oldheader + self.quantfields
 
     def set_feature_generator(self):
         self.features = preparation.add_isoquant_data(self.in_proteins,
                                                       self.quantproteins,
-                                                      self.quantpattern,
-                                                      self.quantheader)
+                                                      self.quantfields)
