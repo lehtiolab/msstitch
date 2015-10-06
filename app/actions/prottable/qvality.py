8111b135b0ab6697ca339dacebd899733124b7cf
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-10-06 13:46:47 +0200

    Added svm as a measure to run qvality on for when it is run on peptide table

--- app/actions/prottable/qvality.py
+++ app/actions/prottable/qvality.py
@@ -1,9 +1,12 @@
 from app.dataformats import prottable as prottabledata
+from app.dataformats import peptable as peptabledata
 
 
-def prepare_qvality_input(features, feattype, get_static_percolator_output=False):
+def prepare_qvality_input(features, feattype,
+                          get_static_percolator_output=False):
     feat_fields = {'probability': prottabledata.HEADER_PROBABILITY,
                    'qvalue': prottabledata.HEADER_QSCORE,
-                  }
+                   'svm': peptabledata.HEADER_SVM_SCORE,
+                   }
     for feature in features:
         yield feature[feat_fields[feattype]]
