94a5c3356277a1c268b94f56231b009c0fb7ef6f
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-10-13 16:43:54 +0200

    Genecentric also for prottable add info

--- app/drivers/prottable/info.py
+++ app/drivers/prottable/info.py
@@ -10,9 +10,11 @@ class AddProteinInfoDriver(ProttableAddData):
         super().__init__(**kwargs)
         self.headertypes = ['proteindata']
         self.poolnames = [kwargs.get('setname')]
+        self.genecentric = kwargs.get('genecentric', False)
 
     def set_feature_generator(self):
         self.features = preparation.add_protein_data(self.in_proteins,
                                                      self.lookup,
                                                      self.headerfields,
+                                                     self.genecentric
                                                      )
