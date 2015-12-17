6386cdfefda13245559eb405ee8dac66a30e8620
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-12-17 14:22:24 +0100

    oops

--- app/drivers/startup.py
+++ app/drivers/startup.py
@@ -1,4 +1,6 @@
-def start_msstitch(drivers, passed_command, args):
+def start_msstitch(drivers, parser, sysargs):
+    passed_command = sysargs[1]
+    args = parser.parse_args(sysargs[2:])
     commandmap = {driver.get_commandname() for driver in drivers}
     command = commandmap[passed_command](**vars(args))
     command.run()
