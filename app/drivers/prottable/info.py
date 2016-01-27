7572da67f3ca772fe98b4b617096e71074995f66
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-01-27 15:36:46 +0000

    Fixed broken addprotein info which didnt use setname thing at all

--- app/drivers/prottable/info.py
+++ app/drivers/prottable/info.py
@@ -8,13 +8,15 @@ class AddProteinInfoDriver(ProttableAddData):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.poolnames = False
         self.headertypes = ['proteindata']
-        self.poolnames = [kwargs.get('setname')]
+        self.setname = kwargs.get('setname')
         self.genecentric = kwargs.get('genecentric', False)
 
     def set_feature_generator(self):
         self.features = preparation.add_protein_data(self.in_proteins,
                                                      self.lookup,
                                                      self.headerfields,
-                                                     self.genecentric
+                                                     self.genecentric,
+                                                     self.setname
                                                      )
