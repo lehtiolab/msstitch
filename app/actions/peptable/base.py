c6e93a51b301cc8c928c7bfb5d58103a9a7b2b70
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-02-25 18:22:23 +0100

    Fixed bug that gave too few linked PSMs per peptide in peptable columnw

--- app/actions/peptable/base.py
+++ app/actions/peptable/base.py
@@ -9,12 +9,15 @@ def add_peptide(allpeps, psm, key, score, fncol=False, new=False):
     if fncol:
         if not new:
             peptide['psms'] = allpeps[key]['psms']
-        peptide['psms'].append('{0}_{1}'.format(psm[fncol],
-                                                psm[psmtsvdata.HEADER_SCANNR]))
     allpeps[key] = peptide
 
 
-def evaluate_peptide(peptides, psm, key, higherbetter, scorecol, fncol=None):
+def add_psm_link(peptide, psm, fncol):
+    peptide['psms'].append('{0}_{1}'.format(psm[fncol],
+                                            psm[psmtsvdata.HEADER_SCANNR]))
+
+
+def evaluate_peptide(peptides, psm, key, higherbetter, scorecol, fncol=False):
     try:
         score = float(psm[scorecol])
     except ValueError:
@@ -29,4 +32,6 @@ def evaluate_peptide(peptides, psm, key, higherbetter, scorecol, fncol=None):
             add_peptide(peptides, psm, key, score, fncol)
         elif not higherbetter and score < existing_score:
             add_peptide(peptides, psm, key, score, fncol)
+    if fncol:
+        add_psm_link(peptides[key], psm, fncol)
     return peptides
