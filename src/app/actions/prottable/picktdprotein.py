3f68468bd1185e3cbac7ef80620ee89411a1c7c7
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2016-10-04 10:48:01 +0200

    Versioned ENSEMBL did not work with gene-map creation from fasta since the fasta-genes are versioned but the biomart ones are not

--- src/app/actions/prottable/picktdprotein.py
+++ src/app/actions/prottable/picktdprotein.py
@@ -49,10 +49,10 @@ def get_score(protein):
 
 
 def create_td_gene_map(tfastafn, dfastafn, fastadelim, genefield):
-    tfasta = (x[1] for x in fasta.get_proteins_genes(tfastafn, fastadelim,
-                                                     genefield))
-    dfasta = (x[1] for x in fasta.get_proteins_genes(dfastafn, fastadelim,
-                                                     genefield))
+    tfasta = (x[1].split('.')[0] for x in
+              fasta.get_proteins_genes(tfastafn, fastadelim, genefield))
+    dfasta = (x[1].split('.')[0] for x in
+              fasta.get_proteins_genes(dfastafn, fastadelim, genefield))
     tdmap = {}
     for target, decoy in zip(tfasta, dfasta):
         tdmap[target] = decoy
