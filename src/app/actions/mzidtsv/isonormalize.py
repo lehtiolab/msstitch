dbf3b529851acb0497e09646623da81824229670
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-03-13 11:36:44 +0100

    Should now be able to generate correct normalized features in the get_isobaric_ratios function.

--- src/app/actions/mzidtsv/isonormalize.py
+++ src/app/actions/mzidtsv/isonormalize.py
@@ -1,11 +1,12 @@
 import sys
+from statistics import median, StatisticsError
 
-from app.actions.isonormalizing import get_medians
 from app.dataformats import prottable as prottabledata
 from app.readers import tsv as reader
 from app.actions import isonormalizing
 
-ISOQUANTRATIO_FEAT_ACC = 'isoquant_target_acc'
+
+ISOQUANTRATIO_FEAT_ACC = '##isoquant_target_acc##'
 
 
 def get_isobaric_ratios(psmfn, psmheader, channels, denom_channels, min_int,
@@ -20,15 +21,18 @@ def get_isobaric_ratios(psmfn, psmheader, channels, denom_channels, min_int,
     psm_or_feat_ratios = get_psmratios(psmfn, psmheader, channels,
                                        denom_channels, min_int, accessioncol)
     if normalize and normratiofn:
-        ch_medians = get_medians(get_psmratios(normratiofn))
-        outratios = generate_normalized_ratios(psm_or_feat_ratios, ch_medians)
+        normheader = reader.get_tsv_header(normratiofn)
+        normratios = get_ratios_from_fn(normratiofn, normheader, channels)
+        ch_medians = get_medians(channels, normratios, report=True)
+        outratios = calculate_normalized_ratios(psm_or_feat_ratios, ch_medians)
     elif normalize:
-        psm_or_feat_ratios = [x for x in psm_or_feat_ratios]
-        ch_medians = get_medians(channels, psm_or_feat_ratios)
-        outratios = generate_normalized_ratios(psm_or_feat_ratios, ch_medians)
+        flatratios = [[feat[ch] for ch in channels]
+                      for feat in psm_or_feat_ratios]
+        ch_medians = get_medians(channels, flatratios, report=True)
+        outratios = calculate_normalized_ratios(psm_or_feat_ratios, ch_medians)
     else:
-        outratios = [x for x in psm_or_feat_ratios]  # FIXME isgenerator input?
-    # outratios = [{ch1: 123, ch2: 456, ISOQUANTRATIO_FEAT_ACC: ENSG1244}]
+        outratios = psm_or_feat_ratios
+    # here: outratios [{ch1: 123, ch2: 456, ISOQUANTRATIO_FEAT_ACC: ENSG1244}]
     if accessioncol and targetfn:
         outratios = {x[ISOQUANTRATIO_FEAT_ACC]: x for x in outratios}
         output_to_target_accession_table(targetfn, outratios)
@@ -40,15 +44,43 @@ def get_isobaric_ratios(psmfn, psmheader, channels, denom_channels, min_int,
 
 
 def get_psmratios(psmfn, header, channels, denom_channels, min_int, acc_col):
+    allfeats, feat_order, psmratios = {}, [], []
     for psm in reader.generate_tsv_psms(psmfn, header):
         ratios = calc_psm_ratios(psm, channels, denom_channels, min_int)
-        psmquant = {ch: str(ratios[ix]) if ratios[ix] != 'NA' else 'NA'
-                    for ix, ch in enumerate(channels)}
-        if acc_col:
-            psmquant[ISOQUANTRATIO_FEAT_ACC] = psm[acc_col]
+        # remove uninformative psms when adding to features
+        if acc_col and (psm[acc_col] == '' or ';' in psm[acc_col] or
+                        not {psm[q] for q in channels}.difference(
+                            {'NA', None, False, ''})):
+            continue
+        elif acc_col:
+            try:
+                allfeats[psm[acc_col]].append(ratios)
+            except KeyError:
+                allfeats[psm[acc_col]] = [ratios]
+            feat_order.append(psm[acc_col])
         else:
+            psmquant = {ch: str(ratios[ix]) if ratios[ix] != 'NA' else 'NA'
+                        for ix, ch in enumerate(channels)}
             psmquant[ISOQUANTRATIO_FEAT_ACC] = False
-        yield psmquant
+            psmratios.append(psmquant)
+    if not acc_col:
+        return psmratios
+    else:
+        outfeatures = []
+        for feat in feat_order:
+            quants = allfeats[feat]
+            outfeature = {ISOQUANTRATIO_FEAT_ACC: feat}
+            outfeature.update(get_medians(channels, quants))
+            outfeature.update(get_no_psms(channels, quants))
+            outfeatures.append(outfeature)
+    return outfeatures
+
+
+def get_ratios_from_fn(fn, header, channels):
+    ratios = []
+    for feat in reader.generate_tsv_psms(fn, header):
+        ratios.append([feat[ch] for ch in channels])
+    return ratios
 
 
 def paste_to_psmtable(psmfn, header, ratios):
@@ -82,6 +114,48 @@ def calc_psm_ratios(psm, channels, denom_channels, min_intensity):
         return ['NA'] * len(channels)
     return [psm_intensity[ch] / denom
             if psm_intensity[ch] != 'NA' else 'NA' for ch in channels]
+
+
+def get_medians(channels, ratios, report=False):
+    ch_medians = {}
+    for ix, channel in enumerate(channels):
+        try:
+            ch_medians[channel] = median([x[ix] for x in ratios
+                                          if x[ix] != 'NA'])
+        except StatisticsError:
+            # channel is empty, common in protein quant but not in normalizing
+            ch_medians[channel] = 'NA'
+    if report:
+        report = ('Channel intensity medians used for normalization:\n'
+                  '{}'.format('\n'.join(['{} - {}'.format(ch, ch_medians[ch])
+                                         for ch in channels])))
+        sys.stdout.write(report)
+    return ch_medians
+
+
+def get_no_psms_field(quantfield):
+    return '{}{}'.format(quantfield, prottabledata.HEADER_NO_PSMS_SUFFIX)
+
+
+def get_no_psms(channels, ratios):
+    ch_nopsms = {}
+    for ix, channel in enumerate(channels):
+        fieldname = get_no_psms_field(channel)
+        ch_nopsms[fieldname] = len([x[ix] for x in ratios if x[ix] != 'NA'])
+    return ch_nopsms
+
+
+def calculate_normalized_ratios(ratios, ch_medians):
+    """Calculates ratios for PSM tables containing isobaric channels with
+    raw intensities. Normalizes the ratios by median. NA values or values
+    below min_intensity are excluded from the normalization."""
+    outratios = []
+    for quant in ratios:
+        channels = [x for x in quant.values() if x != ISOQUANTRATIO_FEAT_ACC]
+        quant.update({ch: str(quant[ch] / ch_medians[ch])
+                      if quant[ch] != 'NA' else 'NA' for ch in channels})
+        outratios.append(quant)
+    return outratios
 
 
 def get_normalized_ratios(psmfn, header, channels, denom_channels,
