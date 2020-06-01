7ff333ce709148f346d95c05890e9b3530c1963d
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2020-06-01 13:43:44 +0200

    Correct module import in decoy creator driver

--- src/app/drivers/sequence.py
+++ src/app/drivers/sequence.py
@@ -1,5 +1,6 @@
 from app.drivers import base
-from app.actions import sequence as preparation
+from app.actions import sequence
+from app.actions.lookups import sequence as seqlup
 from app.drivers.options import lookup_options, sequence_options
 from Bio import SeqIO
 
@@ -29,8 +30,8 @@ class DecoySeqDriver(base.BaseDriver):
         outfn = self.create_outfilepath(self.fn, self.outsuffix)
         if self.lookup is None and not self.ignoretarget:
             self.initialize_lookup('decoychecker.sqlite')
-            preparation.create_searchspace(self.lookup, self.fn, self.minlength, reverse_seqs=False, miss_cleavage=self.miss_cleavage)
-        decoyfa = preparation.create_decoy_fa(self.fn, self.scramble, self.lookup, self.trypsinize, self.miss_cleavage, self.minlength, self.max_shuffle)
+            seqlup.create_searchspace(self.lookup, self.fn, self.minlength, reverse_seqs=False, miss_cleavage=self.miss_cleavage)
+        decoyfa = sequence.create_decoy_fa(self.fn, self.scramble, self.lookup, self.trypsinize, self.miss_cleavage, self.minlength, self.max_shuffle)
         with open(outfn, 'w') as fp:
             SeqIO.write(decoyfa, fp, 'fasta')
 
@@ -53,5 +54,5 @@ class TrypsinizeDriver(base.BaseDriver):
         outfn = self.create_outfilepath(self.fn, self.outsuffix)
         with open(self.fn) as fp, open(outfn, 'w') as wfp:
             seqs = SeqIO.parse(fp, 'fasta')
-            decoyfa = preparation.create_trypsinized(seqs, self.proline, self.miss_cleavage, self.minlength)
+            decoyfa = sequence.create_trypsinized(seqs, self.proline, self.miss_cleavage, self.minlength)
             SeqIO.write(decoyfa, wfp, 'fasta')
