51688094353b45e12e6cb8521fe480ae7568106d
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2023-11-13 10:14:47 +0100

    Rework perco2psm to deal with PSM tables which are not exactly synced with mzid files due to filtering of PSM table

--- src/app/actions/psmtable/percolator.py
+++ src/app/actions/psmtable/percolator.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import re
 from app.readers import mzidplus as readers
 from app.readers import tsv as tsvreader
 from app.readers import xml
@@ -73,56 +75,84 @@ def get_scores_fdrs_from_percoxml(percofn):
     return psms
 
 
+def generate_spec_id_items(mzid_specidr, mzns):
+    scan = 0
+    has_scans = True
+    for specidr in mzid_specidr:
+        specid = specidr.attrib['spectrumID']
+        if has_scans:
+            try:
+                scan = int({x.split('=')[0]: x.split('=')[1] for x in specidr.attrib['spectrumID'].split(' ')}['scan'])
+            except KeyError:
+                # in e.g. timstof data there are no true scan numbers, percolator sets it by increment
+                has_scans = False
+                scan += 1
+        else:
+            scan += 1
+        for specidi in specidr.findall('{%s}SpectrumIdentificationItem' % mzns['xmlns']):
+            yield scan, specid, specidi
+
+
 def add_fdr_to_mzidtsv(psms, mzid_specidr, mzns, percodata):
     """Takes PSMs from an mzIdentML and its MSGF+ TSV and a corresponding 
     percolator XML. 
     """
     # mzId results and PSM lines can be zipped
-    scan = 0
-    for specidr in mzid_specidr:
+    # but we assume that multi-solution scans (e.g. Isoleucine/leucine)
+    # have identical scoring, i.e. MSGF is run with -n1 (default)
+
+    specidis = generate_spec_id_items(mzid_specidr, mzns)
+    for psm in psms:
+        psmspecid = psm[psmheaders.HEADER_SPECSCANID]
+        psmseq = re.sub('[^A-Z]', '', psm[psmheaders.HEADER_PEPTIDE].upper())
+        psmcharge = psm[psmheaders.HEADER_CHARGE]
+
+        mzid_specid = False
+        spfile = os.path.splitext(psm[psmheaders.HEADER_SPECFILE])[0]
+        percopsm = False
+        while mzid_specid != psmspecid:
+            try:
+                scan, mzid_specid, specidi = next(specidis)
+            except StopIteration:
+                print('PSM tsv file and corresponding mzIdentML are not lined '
+                        'up properly')
+                sys,exit(1)
+            else:
+                perco_id = f'{spfile}_{specidi.attrib["id"]}_{scan}_{psmcharge}_{specidi.attrib["rank"]}'
+                try:
+                    percopsm = percodata[perco_id]
+                except KeyError:
+                    pass
+        if not percopsm:
+            continue
+        outpsm = {k: v for k,v in psm.items()}
+        psmscores = {
+            psmheaders.HEADER_SVMSCORE: percopsm['svm'], 
+            psmheaders.HEADER_PSMQ: percopsm['qval'], 
+            psmheaders.HEADER_PSM_PEP: percopsm['pep'], 
+            }
         try:
-            scan = int({x.split('=')[0]: x.split('=')[1] for x in specidr.attrib['spectrumID'].split(' ')}['scan'])
-        except KeyError:
-            # in e.g. timstof data there are no true scan numbers, percolator sets it by increment
-            scan += 1
-        for specidi in specidr.findall('{%s}SpectrumIdentificationItem' % mzns['xmlns']):
-            psm = next(psms)
-            # percolator psm ID is: samplename_SII_scanindex_sii.ix_scannr_charge_rank
-            # SII_scanindex_sii.ix is in SII.attrib['id']
-            spfile = os.path.splitext(psm[psmheaders.HEADER_SPECFILE])[0]
-            try:
-                percopsm = percodata['{fn}_{sii_id}_{sc}_{ch}_{rk}'.format(fn=spfile,
-                    sii_id=specidi.attrib['id'], sc=scan, rk=specidi.attrib['rank'], ch=psm['Charge'])]
-            except KeyError:
-                continue
-            outpsm = {k: v for k,v in psm.items()}
-            psmscores = {
-                psmheaders.HEADER_SVMSCORE: percopsm['svm'], 
-                psmheaders.HEADER_PSMQ: percopsm['qval'], 
-                psmheaders.HEADER_PSM_PEP: percopsm['pep'], 
+            pepscores = {
+                psmheaders.HEADER_PEPTIDE_Q: percopsm['pepqval'], 
+                psmheaders.HEADER_PEPTIDE_PEP: percopsm['peppep'], 
+                psmheaders.HEADER_TARGETDECOY: percopsm['td'],
                 }
-            try:
-                pepscores = {
-                    psmheaders.HEADER_PEPTIDE_Q: percopsm['pepqval'], 
-                    psmheaders.HEADER_PEPTIDE_PEP: percopsm['peppep'], 
-                    psmheaders.HEADER_TARGETDECOY: percopsm['td'],
-                    }
-            except KeyError:
-                pepscores = {
-                    psmheaders.HEADER_PEPTIDE_Q: 1,
-                    psmheaders.HEADER_PEPTIDE_PEP: 1,
-                    psmheaders.HEADER_TARGETDECOY: 1,
-                    }
-            outpsm.update({**psmscores, **pepscores})
-            # Remove all decoy protein matches from target proteins, to ensure downstream
-            # processing does not trip up on them, e.g. having a decoy master protein.
-            if percopsm['td'] != 'decoy':
-                outprots = []
-                for prot in outpsm[psmheaders.HEADER_PROTEIN].split(';'):
-                    if not prot.startswith(psmheaders.DECOY_PREFIX):
-                        outprots.append(prot)
-                outpsm[psmheaders.HEADER_PROTEIN] = ';'.join(outprots)
-            yield outpsm
+        except KeyError:
+            pepscores = {
+                psmheaders.HEADER_PEPTIDE_Q: 1,
+                psmheaders.HEADER_PEPTIDE_PEP: 1,
+                psmheaders.HEADER_TARGETDECOY: 1,
+                }
+        outpsm.update({**psmscores, **pepscores})
+        # Remove all decoy protein matches from target proteins, to ensure downstream
+        # processing does not trip up on them, e.g. having a decoy master protein.
+        if percopsm['td'] != 'decoy':
+            outprots = []
+            for prot in outpsm[psmheaders.HEADER_PROTEIN].split(';'):
+                if not prot.startswith(psmheaders.DECOY_PREFIX):
+                    outprots.append(prot)
+            outpsm[psmheaders.HEADER_PROTEIN] = ';'.join(outprots)
+        yield outpsm
 
 
 def get_header_with_percolator(oldheader):
